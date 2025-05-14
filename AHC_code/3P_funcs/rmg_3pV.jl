8f307ce0a025917d27cb13cd969b579c94616fa7
Avi <akvadali@gmail.com>
2025-05-14 11:47:44 -0700

    updating

--- AHC_code/3P_funcs/rmg_3pV.jl
+++ AHC_code/3P_funcs/rmg_3pV.jl
@@ -24,19 +24,25 @@ function limit_delta_all(Nl)
 end
 function rmg_alpha_all(kappa, nu, Nl)
     omega = exp(im * 2 * pi / 3)
-    denom = 10^(-16)
     sum1 = 0
     sum2 = 0
     sum3 = 0
     sum4 = 0
     for j in 0:(Nl - 1)
-        denom += nu^(2*j) * kappa^(2*j)
-        sum1 += nu^(2*j) * kappa^(2*j) * omega^j * j
-        sum2 += nu^(2*j) * kappa^(2*j) * omega^j
-        sum3 += nu^(2*j) * kappa^(2*j) * j
-        sum4 += nu^(2*j) * kappa^(2*j)
+        sum1 += nu^(2*j) * kappa^(2*j)
+        sum2 += j * nu^(2*j) * kappa^(2*j - 1)
+        sum3 += omega^j * nu^(2*j) * kappa^(2*j)
+        sum4 += 2 * j * omega * omega^j * nu^(2*j) * kappa^(2*j - 1) 
     end
-    return (-1/kappa) * (2 * conj(omega) * sum1 + sum2 * sum3 / sum4) / denom
+    return (sum2 * sum3)/sum1^2 + sum4/sum1
+end
+function closed_rmg_alpha_all(kappa, nu, Nl)
+    omega = exp(im * 2 * pi / 3)
+    return (1/kappa * ((1 - nu^2 * kappa^2)/(1 - (nu^2 * kappa^2)^Nl))^2 * (1/(1 - nu^2 * kappa^2)^2 * 
+    (nu^2 * kappa^2 - Nl * (nu^2 * kappa^2)^Nl + (Nl - 1) * (nu^2 * kappa^2)^(Nl + 1))) * 
+    (1/(1 - omega * nu^2 * kappa^2) * (1 - (omega * nu^2 * kappa^2)^Nl)) + 2*omega/kappa * 
+    (1 - nu^2 * kappa^2)/(1 - (nu^2 * kappa^2)^Nl) * 1/(1 - omega * nu^2 * kappa^2)^2 * 
+    (omega * nu^2 * kappa^2 - Nl * (omega * nu^2 * kappa^2)^Nl + (Nl - 1) * (omega * nu^2 * kappa^2)^(Nl + 1)))
 end
 function limit_alpha_all(kappa, Nl)
     omega = exp(im * 2 * pi / 3)
@@ -104,77 +110,129 @@ function bc_sign_alpha_delta(m_kappa, spacing, Nl, nu, alpha, delta, q)
     end
 end
 
-function rmg_delta_linear_layer(kappa, nu, Nl, layer)
+function rmg_delta_linear(kappa, nu, Nl)
     omega = exp(im * 2 * pi / 3)
-    nmz = 10^(-16)
-    num = 10^(-16)
+    dn = 10^(-16)
+    nm = 10^(-16)
     for j in 0:(Nl - 1)
-        nmz += nu^(2*j) * kappa^(2*j)
+        dn += nu^(2*j) * kappa^(2*j)
+        nm += nu^(2*j) * kappa^(2*j) * omega^j * (j)
     end
-    l = layer - 1
-    num = -(l + 1) * nu^(2*l) * kappa^(2*l) * omega^l
-    return num / nmz
+    return nm / dn
 end
 
-function rmg_alpha_linear_layer(kappa, nu, Nl, layer)
+function rmg_alpha_linear(kappa, nu, Nl)
     omega = exp(im * 2 * pi / 3)
-    denom = 10^(-16)
-    sum1 = 10^(-16)
-    sum2 = 10^(-16)
-    sum3 = 10^(-16)
-    sum4 = 10^(-16)
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
     for j in 0:(Nl - 1)
-        denom += nu^(2*j) * kappa^(2*j)
-        sum3 += nu^(2*j) * kappa^(2*j) * j
-        sum4 += nu^(2*j) * kappa^(2*j)
+        sum1 += nu^(2*j) * kappa^(2*j)
+        sum2 += j * nu^(2*j) * kappa^(2*j - 1)
+        sum3 += omega^j * nu^(2*j) * kappa^(2*j) * (j)
+        sum4 += 2 * j * omega * omega^j * nu^(2*j) * kappa^(2*j - 1) * (j)
     end
-    l = layer - 1
-    sum1 = nu^(2*l) * kappa^(2*l) * omega^l * l
-    sum2 = nu^(2*l) * kappa^(2*l) * omega^l
-    return -(l + 1) * (-1/kappa) * (2 * conj(omega) * sum1 + sum2 * sum3 / sum4) / denom
+    return (sum2 * sum3)/sum1^2 + sum4/sum1
 end
 
-function rmg_delta_exp_layer(kappa, nu, Nl, layer)
+function rmg_delta_exp(kappa, nu, Nl)
     omega = exp(im * 2 * pi / 3)
-    nmz = 10^(-16)
-    num = 10^(-16)
+    dn = 10^(-16)
+    nm = 10^(-16)
     for j in 0:(Nl - 1)
-        nmz += nu^(2*j) * kappa^(2*j)
+        dn += nu^(2*j) * kappa^(2*j)
+        nm += nu^(2*j) * kappa^(2*j) * omega^j * exp(3*j)
     end
-    l = layer - 1
-    num = -exp(-l - 1) * nu^(2*l) * kappa^(2*l) * omega^l
-    return num / nmz
+    return nm / dn
 end
 
-function rmg_alpha_exp_layer(kappa, nu, Nl, layer)
+function rmg_alpha_exp(kappa, nu, Nl)
     omega = exp(im * 2 * pi / 3)
-    denom = 10^(-16)
-    sum1 = 10^(-16)
-    sum2 = 10^(-16)
-    sum3 = 10^(-16)
-    sum4 = 10^(-16)
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    sum4 = 0
     for j in 0:(Nl - 1)
-        denom += nu^(2*j) * kappa^(2*j)
-        sum3 += nu^(2*j) * kappa^(2*j) * j
-        sum4 += nu^(2*j) * kappa^(2*j)
+        sum1 += nu^(2*j) * kappa^(2*j)
+        sum2 += j * nu^(2*j) * kappa^(2*j - 1)
+        sum3 += omega^j * nu^(2*j) * kappa^(2*j) * exp(3*j)
+        sum4 += 2 * j * omega * omega^j * nu^(2*j) * kappa^(2*j - 1) * exp(3*j)
     end
-    l = layer - 1
-    sum1 = nu^(2*l) * kappa^(2*l) * omega^l * l
-    sum2 = nu^(2*l) * kappa^(2*l) * omega^l
-    return -exp(-l - 1) * (-1/kappa) * (2 * conj(omega) * sum1 + sum2 * sum3 / sum4) / denom
+    return (sum2 * sum3)/sum1^2 + sum4/sum1
 end
 
+# function rmg_delta_linear_layer(kappa, nu, Nl, layer)
+#     omega = exp(im * 2 * pi / 3)
+#     nmz = 10^(-16)
+#     num = 10^(-16)
+#     for j in 0:(Nl - 1)
+#         nmz += nu^(2*j) * kappa^(2*j)
+#     end
+#     l = layer - 1
+#     num = -(l + 1) * nu^(2*l) * kappa^(2*l) * omega^l
+#     return num / nmz
+# end
 
-function rmg_inf_layer_delta(kappa, nu, layer, g)
-    omega = exp(im * 2 * pi / 3)
-    l = layer - 1
-    return omega^l * g * (kappa * nu)^(2 * l) * (1 - kappa^2 * nu^2)
-end
+# function rmg_alpha_linear_layer(kappa, nu, Nl, layer)
+#     omega = exp(im * 2 * pi / 3)
+#     denom = 10^(-16)
+#     sum1 = 10^(-16)
+#     sum2 = 10^(-16)
+#     sum3 = 10^(-16)
+#     sum4 = 10^(-16)
+#     for j in 0:(Nl - 1)
+#         denom += nu^(2*j) * kappa^(2*j)
+#         sum3 += nu^(2*j) * kappa^(2*j) * j
+#         sum4 += nu^(2*j) * kappa^(2*j)
+#     end
+#     l = layer - 1
+#     sum1 = nu^(2*l) * kappa^(2*l) * omega^l * l
+#     sum2 = nu^(2*l) * kappa^(2*l) * omega^l
+#     return -(l + 1) * (-1/kappa) * (2 * conj(omega) * sum1 + sum2 * sum3 / sum4) / denom
+# end
 
-function rmg_inf_layer_alpha(kappa, nu, layer, g)
-    omega = exp(im * 2 * pi / 3)
-    l = layer - 1
-    return ((-1/kappa) * omega^l * g * (kappa * nu)^(2 * l) * (kappa^2 * nu^2 + 
-    2 * conj(omega) * l * (1 - kappa^2 * nu^2))) 
-end
+# function rmg_delta_exp_layer(kappa, nu, Nl, layer)
+#     omega = exp(im * 2 * pi / 3)
+#     nmz = 10^(-16)
+#     num = 10^(-16)
+#     for j in 0:(Nl - 1)
+#         nmz += nu^(2*j) * kappa^(2*j)
+#     end
+#     l = layer - 1
+#     num = -exp(-l - 1) * nu^(2*l) * kappa^(2*l) * omega^l
+#     return num / nmz
+# end
+
+# function rmg_alpha_exp_layer(kappa, nu, Nl, layer)
+#     omega = exp(im * 2 * pi / 3)
+#     denom = 10^(-16)
+#     sum1 = 10^(-16)
+#     sum2 = 10^(-16)
+#     sum3 = 10^(-16)
+#     sum4 = 10^(-16)
+#     for j in 0:(Nl - 1)
+#         denom += nu^(2*j) * kappa^(2*j)
+#         sum3 += nu^(2*j) * kappa^(2*j) * j
+#         sum4 += nu^(2*j) * kappa^(2*j)
+#     end
+#     l = layer - 1
+#     sum1 = nu^(2*l) * kappa^(2*l) * omega^l * l
+#     sum2 = nu^(2*l) * kappa^(2*l) * omega^l
+#     return -exp(-l - 1) * (-1/kappa) * (2 * conj(omega) * sum1 + sum2 * sum3 / sum4) / denom
+# end
+
+
+# function rmg_inf_layer_delta(kappa, nu, layer, g)
+#     omega = exp(im * 2 * pi / 3)
+#     l = layer - 1
+#     return omega^l * g * (kappa * nu)^(2 * l) * (1 - kappa^2 * nu^2)
+# end
+
+# function rmg_inf_layer_alpha(kappa, nu, layer, g)
+#     omega = exp(im * 2 * pi / 3)
+#     l = layer - 1
+#     return ((-1/kappa) * omega^l * g * (kappa * nu)^(2 * l) * (kappa^2 * nu^2 + 
+#     2 * conj(omega) * l * (1 - kappa^2 * nu^2))) 
+# end
 
